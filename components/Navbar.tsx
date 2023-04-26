42bbaeb3ca9f36b69a0d565f797b7c182b5a60ec
ik04 <ishaankhurana123@gmail.com>
2023-04-26 14:39:31 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+type Navlink = {
+  name: string;
+  href: string;
+};
+let navLinks: Navlink[];
+navLinks = [
+  { name: "Home", href: "/" },
+  { name: "Company", href: "/" },
+  { name: "Resources", href: "/" },
+  { name: "About", href: "/" },
+  { name: "Contact", href: "/" },
+];
+
+export const Navbar = () => {
+  const [nav, setNav] = useState<boolean>(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-green-500">REACT.</h1>
+      <ul className="hidden md:flex">
+        {navLinks.map((navLink) => (
+          <a href={navLink.href}>
+            <li className="p-4">{navLink.name}</li>
+          </a>
+        ))}
+      </ul>
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      <div
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full m-4 text-3xl font-bold text-green-500">REACT.</h1>
+        <ul className="uppercase p-4">
+          {navLinks.map((navLink) => (
+            <a href={navLink.href}>
+              <li className="p-4 border-b border-gray-600">{navLink.name}</li>
+            </a>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
